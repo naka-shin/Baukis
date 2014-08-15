d590989cae22785917ffd0912120cc4b9933e11a
nakashin <nakashin@HP-EliteBook-6930p>
2014-08-15 10:03:23 +0900

    2014-08-15

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,30 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :set_layout
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+
+  rescue_from Exception, with: :rescue500
+
+  private
+  def set_layout
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
+      Regexp.last_match[1]
+    else
+      'customer'
+    end
+  end
+
+  def rescue404(e)
+    @exception = e
+    render template: 'errors/not_found', status: 404
+  end
+
+  def rescue500(e)
+    @exception = e
+    render 'errors/internal_server_error', status: 500
+  end
 end
+
