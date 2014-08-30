3dd67b63902451cadfa022109a3d631a89c0eeca
vagrant <vagrant@localhost.localdomain>
2014-08-30 16:38:46 +0900

    2014-08-29

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue404
 
-  rescue_from Exception, with: :rescue500
+   #rescue_from Exception, with: :rescue500
   rescue_from Forbidden, with: :rescue403
   rescue_from IpAddressRejected, with: :rescue403
 
